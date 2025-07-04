effde0a5c3baab36ade24ded44cbccb4ec56a1be
Binarma <binarma@gmail.com>
2025-07-04 10:27:28 +0545

    ui enhancehement

--- script.js
+++
@@ -1,7 +0,0 @@
- const button =document.getElementById("darkmode");
- 
- 
- button.addEventListener('click',function toggleMode() {
-      document.body.classList.toggle("dark-mode");
-      console.log("clicked")
-    })
